a911dc90580dd86dbc179662c183b064e33b5798
Anarco Nascimento <anarco.nascimento@ccc.ufcg.edu.br>
2021-02-17 13:31:20 -0300

    Spinner de Loading

---
+++ src/components/Loading/index.js
@@ -0,0 +1,27 @@
+import styled, { keyframes } from 'styled-components';
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Loading = styled.div`
+  animation: ${rotate360} 1s linear infinite;
+  transform: translateZ(0);
+  
+  border-top: 2px solid grey;
+  border-right: 2px solid grey;
+  border-bottom: 2px solid grey;
+  border-left: 4px solid royalblue;
+  background: transparent;
+  width:2vw;
+  height: 2vw;
+  border-radius: 50%;
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+`;
